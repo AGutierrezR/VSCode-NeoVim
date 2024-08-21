ea4951559230939ddc9aeaaeae29812d90455aea
Andres Gutierrez Ramirez <agutierrez.codes@gmail.com>
2024-08-21 19:48:59 +0200

    Initial Commit (Initial Configuration)

---
+++ init.lua
@@ -0,0 +1,16 @@
+-- <leader> key
+vim.g.mapleader = ' '
+vim.g.maplocalleader = ' '
+
+-- sync system clipboard
+vim.opt.clipboard = 'unnamedplus'
+
+-- Case-insensitive searching UNLESS \C or one or more capital letters in the search term
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+
+-- clear search highlighting
+vim.keymap.set('n', '<Esc>', ':nohlsearch<cr>')
+
+-- paste without overwriting
+vim.keymap.set('v', 'p', 'P')
