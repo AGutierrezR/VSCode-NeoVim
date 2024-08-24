6e42ae0f21aaf4e07fc395abe9863313e8c40af8
Andres Gutierrez <agutierrez.codes@gmail.com>
2024-08-24 20:58:56 +0200

    Improve treesitter configuration

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -3,12 +3,24 @@ return {
     "nvim-treesitter/nvim-treesitter",
     config = function()
       require("nvim-treesitter.configs").setup({
-        ensure_installed = { "lua", "vim", "javascript", "typescript", "json" },
+        ensure_installed = { "lua", "vim", "javascript", "typescript", "json", "html" },
         
         highlight = {
           enable = true,
-        }
+        },
+        
+        -- Se
+        incremental_selection = {
+          enable = true,
+          keymaps = {
+            node_incremental = "v",
+            node_decremental = "V",
+          }
+        },
       })
+      
+      -- Configuration to treat JSONC files as JSON
+      vim.treesitter.language.register('json', 'jsonc')
     end, 
   },
   "nvim-treesitter/nvim-treesitter-textobjects"
