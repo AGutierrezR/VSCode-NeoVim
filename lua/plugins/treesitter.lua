ebd95dede2ffafd47ff09d69a4c3c5f54980a4d4
Andres Gutierrez <agutierrez.codes@gmail.com>
2024-09-10 23:23:20 +0200

    Add more languages to treesitter

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -3,7 +3,7 @@ return {
     "nvim-treesitter/nvim-treesitter",
     config = function()
       require("nvim-treesitter.configs").setup({
-        ensure_installed = { "lua", "vim", "javascript", "typescript", "json", "html" },
+        ensure_installed = { "lua", "vim", "javascript", "typescript", "json", "html", "markdown", "markdown_inline" },
         
         highlight = {
           enable = true,
