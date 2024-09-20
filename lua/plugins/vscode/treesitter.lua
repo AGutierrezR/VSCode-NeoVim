275f9d50ece33f9a5f05f318d60ff8f92d2cf5dc
Andres Gutierrez Ramirez <a.gutierrez.ramirez.contractor@bbva.com>
2024-09-21 01:03:01 +0200

    Add treesitter textobject-move (functions, classes)

--- lua/plugins/vscode/treesitter.lua
+++ lua/plugins/vscode/treesitter.lua
@@ -17,10 +17,45 @@ return {
             node_decremental = "V",
           }
         },
+        
+        textobjects = {
+          move = {
+            enable = true,
+            set_jumps = true,
+            goto_next_start = {
+              ["]f"] = "@function.outer",
+              ["]]"] = "@class.outer",
+            },
+            goto_next_end = {
+              ["]F"] = "@function.outer",
+              ["]["] = "@class.outer",
+            },
+            goto_previous_start = {
+              ["[f"] = "@function.outer",
+              ["[["] = "@class.outer",
+            },
+            goto_previous_end = {
+              ["[F"] = "@function.outer",
+              ["[]"] = "@class.outer",
+            },
+          },
+        }
       })
-      
       -- Configuration to treat JSONC files as JSON
       vim.treesitter.language.register('json', 'jsonc')
+      
+      local ts_repeat_move = require "nvim-treesitter.textobjects.repeatable_move"
+
+      -- Repeat movement with ; and ,
+      -- ensure ; goes forward and , goes backward regardless of the last direction
+      vim.keymap.set({ "n", "x", "o" }, ";", ts_repeat_move.repeat_last_move_next)
+      vim.keymap.set({ "n", "x", "o" }, ",", ts_repeat_move.repeat_last_move_previous)
+
+      -- Make builtin f, F, t, T also repeatable with ; and ,
+      vim.keymap.set({ "n", "x", "o" }, "f", ts_repeat_move.builtin_f_expr, { expr = true })
+      vim.keymap.set({ "n", "x", "o" }, "F", ts_repeat_move.builtin_F_expr, { expr = true })
+      vim.keymap.set({ "n", "x", "o" }, "t", ts_repeat_move.builtin_t_expr, { expr = true })
+      vim.keymap.set({ "n", "x", "o" }, "T", ts_repeat_move.builtin_T_expr, { expr = true })
     end, 
   },
   "nvim-treesitter/nvim-treesitter-textobjects"
