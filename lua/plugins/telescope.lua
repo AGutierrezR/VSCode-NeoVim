9f15f5d837f2f44bd9a7de36220464dc55e26fdc
Andres Gutierrez <agutierrez.codes@gmail.com>
2024-10-20 14:14:16 +0200

    Add alpha for dashboard, session, and telescope fzf

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -5,7 +5,8 @@ return {
     event = 'VeryLazy',
     dependencies = {
       'nvim-lua/plenary.nvim',
-      { 'nvim-telescope/telescope-ui-select.nvim' }
+      { 'nvim-telescope/telescope-ui-select.nvim' },
+      { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' },
     },
     config = function()
       require('telescope').setup({
@@ -18,12 +19,14 @@ return {
 
       -- Enable telescope extension if they are installed
       pcall(require('telescope').load_extension('ui-select'))
+      pcall(require('telescope').load_extension('fzf'))
 
       -- See `:help telescope.builtin`
       local builtin = require('telescope.builtin')
       vim.keymap.set('n', '<leader>f', builtin.find_files, { desc = 'Search File' })
       vim.keymap.set('n', '<leader>/', builtin.live_grep, { desc = 'Search by Grep' })
       vim.keymap.set('n', '<leader>b', builtin.buffers, { desc = 'Find existing buffers' })
+      vim.keymap.set('n', '<leader>?', builtin.commands, { desc = 'Find existing buffers' })
 
     end
   },
