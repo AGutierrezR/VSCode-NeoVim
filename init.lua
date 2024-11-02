7d6e3a9f11c6061b05540d3d076aab854b5ebb8a
Andres Gutierrez <agutierrez.codes@gmail.com>
2024-11-02 01:05:42 +0100

    Remove unnecessary runtimepath

--- init.lua
+++ init.lua
@@ -1,7 +1,3 @@
--- Add the new configuration directory to Neovim's runtime path.
--- Only needed if Neovim's configuration is located outside of ~/.config/nvim
-vim.opt.runtimepath:append("~/.config/neocode")
-
 -- Import initial user settings (e.g., interface and behavior).
 require('user.settings')
 
